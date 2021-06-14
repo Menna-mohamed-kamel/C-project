0787f4223aa7c10143664f47d91134f9dee42a26
menna <menna@feng.bu.edu.eg>
2021-06-14 22:41:39 +0200

    made a function for display list ,delete from list and know how many elements in list.

--- Project.c
+++ Project.c
@@ -11,9 +11,13 @@ typedef struct node_book {
     struct node_book *next;
 }Node;
 
-Node *head = NULL, *end = NULL;
+Node *head = NULL, *end = NULL, *curr = NULL;
 
 void Append();
+void printList();
+int length();
+void print_node(Node *);
+void deleteNode();
 
 
 typedef struct Book
@@ -60,3 +64,67 @@ printf("done\n\n");
         printf("end\n\n");
     fclose(file);
  }
+
+
+void printList() {
+   curr=head;
+
+   //start from the beginning
+   while(curr!=NULL) {
+      printf("name: %-12s\tcategory: %-12s\tcode number: %3d\tprice: %7.2f\tcopies: %2d\n",curr->name,curr->category,curr->code,curr->price, curr->copies);
+      curr = curr->next;
+   }
+
+}
+
+
+int length() {
+   int length=0;
+   Node *leng;
+leng=head;
+   while(leng!=NULL){
+    length++;
+    leng=leng->next;
+   }
+
+   return length;
+}
+
+void print_node(Node *current){
+      printf("name: %-12s\tcategory: %-12s\tcode number: %3d\tprice: %7.2f\tcopies: %2d\n",current->name,current->category,current->code,current->price, current->copies);
+ }
+
+void deleteNode(){
+
+   if(head == NULL) {
+      printf("\nthe library is empty\n\n");
+      return;
+   }
+
+   int code;
+   printf("please enter the code of the book: ");
+   scanf("%d", &code);
+
+   Node *pre=NULL, *current=head;
+
+   while(current->code != code) {
+      if(current->next == NULL){
+         printf("\nthis code is invalid\n");
+         return;
+      }
+       else
+         {
+         pre = current;
+         current = current->next;
+         }
+   }
+   printf("\nfound book\n");
+
+    print_node(current);
+
+    if(current==head)head=head->next;
+    else pre->next = current->next;
+
+    free(current);
+}
+
